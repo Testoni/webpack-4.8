bffb53919384d6d262698987a81d988378de72dd
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-07-07 21:47:41 -0300

    first config

---
+++ src/main.js
@@ -0,0 +1,7 @@
+import Person from './person'
+
+const p = new Person
+
+console.log(p.greet())
+
+// npm start
---
+++ webpack.config.js
@@ -0,0 +1,6 @@
+const webpack = require('webpack')
+
+module.exports = {
+    mode: 'development', // production
+    entry: './src/main.js'
+}
