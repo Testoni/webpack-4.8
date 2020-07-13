3b0cacaf6c4ab043190bf60e02a887f5d0825e10
Gabriel Testoni <gabriel.testoni@gmail.com>
2020-07-13 09:24:44 -0300

    development and production, optimize css assest, uglify and cross env dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,24 @@
+const modeDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const Uglify = require('uglify-webpack-plugin')
+const OptimizeCssAssetsPlugin = require('optimiza-css-assests-webpack-plugin')
 
 module.exports = {
-    mode: 'development', // production
+    mode: modeDev ? 'development' : 'production', // production - npm run build / npm run dev
     entry: './src/main.js',
     output: {
         filename: 'main.js',
         path: __dirname + '/public'
+    },
+    optimization: {
+        minimizer: [
+            new Uglify({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCssAssetsPlugin({})
+        ]
     },
     plugins: [
         new MiniCssExtractPlugin({
